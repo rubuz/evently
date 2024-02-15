deee95834fee24d04c9c2db71e427661094adaf9
rubuz <rubuz23@gmail.com>
2024-02-15 13:57:26 +0100

    Add logging for WEBHOOK_SECRET and user object

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -9,6 +9,10 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
+  if (WEBHOOK_SECRET) {
+    console.log("WEBHOOK_SECRET", WEBHOOK_SECRET);
+  }
+
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
@@ -91,6 +95,8 @@ export async function POST(req: Request) {
       username: username!,
       photo: image_url,
     };
+
+    console.log("user", user);
 
     const updatedUser = await updateUser(id, user);
 
